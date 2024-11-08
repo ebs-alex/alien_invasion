836e30a3203299f26d19c75757c6098aaa5b24bf
Alex Moran <ebs.alex@icloud.com>
2024-11-08 12:23:03 -0800

    added ship, added first motion, refactored code with helper methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall Class to manage game assets and resources"""
@@ -15,23 +16,35 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # set the background color
         self.bg_color = (self.settings.bg_color)
 
     def run_game(self):
         """Start main game loop"""
         while True:
-            #watch for keyboard and mouse events
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+           #watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                     if event.key == pygame.K_RIGHT:
+                          # Move ship to the right
+                          self.ship.rect.x += 1
 
+    def _update_screen(self):
             # redraw the screen during each pass through the loop
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
 
             #Make most recently drawn screen visible
             pygame.display.flip()
-
+         
 if __name__ == '__main__':
     # Make a game instance and run game
     ai = AlienInvasion()
